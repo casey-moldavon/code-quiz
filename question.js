d169f786f8d1932f4591c6ed17e56b84e8c50c3d
Casey Yang <cmoldavon@gmail.com>
2019-12-06 01:57:42 -0800

    Adding script.js framework

---
+++ question.js
@@ -0,0 +1,19 @@
+function questions(text, options, answer) {
+    this.text=text;
+    this.options = options;
+    this.answer = answer;
+}
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      options: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    ///etc.
+  ];
--- script.js
+++ script.js
@@ -1,143 +1,84 @@
+
+//these are the quiz options
+var questionDisplay = document.querySelector("#question-display");
+var option1 = document.getElementById("op1");
+var option2 = document.getElementById("op2");
+var option3 = document.getElementById("op3");
+var option4 = document.getElementById("op4");
 var startButton = document.getElementById("start-button");
-var questionList = document.getElementById('question-list');
-
-startButton.addEventListener("click", startGame);
-
-function startGame() {
-    startButton.classList.add('hide');
-}
-
-function nextQuestion() {
-}
-
-function selectAsnwer() {
-}
-
-
-
-
-
-
-var qA = document.getElementById("a");
-var qB = document.getElementById("b");
-var qC = document.getElementById("c");
-var qD = document.getElementById("d");
-var startGame = document.querySelector("#button");
-
+var timeEl = document.querySelector(".timer");
 var score = 0
 
 
-for (var i=0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-    if (response === questions[i].answer) {
-        score ++;
-        alert("Correct!");
+//-------------------------- function to start quiz ---------------------------------
+startButton.addEventListener("click", function() {
+    console.log("happy thoughts");
+    quizQuestions();
+    setTime();
+    startButton.style.visibility = "hidden";
+})
+
+
+//----------------------------------- Timer -----------------------------------------
+
+var secondsLeft = 76; //starting seconds
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft + " seconds left";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      alert("time is up");
     }
-    else {
-        alert("Wrong!");
-    }
+
+  }, 1000);
 }
-alert("You got " + score + "/" + questions.length);
 
 
-// function questions() {
-//     var index = 0
-//     questions[0].title;
-//     qA.innerHTML = questions[0].choices[0];
-//     aB.innerHTML = questions[0].choices[1];
-//     qC.innerHTML = questions[0].choices[2];
-//     qD.innerHTML = questions[0].choices[3];
-//     index ++
-// };
+// ------------------------------ Quiz Questions ------------------------------------
+
+function quizQuestions() {
+    var index = 0
+    questionDisplay = questions[0].title;
+    option1 = questions[0].options[0];
+    option2 = questions[0].options[1];
+    option3 = questions[0].options[2];
+    option4 = questions[0].options[3];
+    index ++
+
+    // var input = //this variable represents the user's answer
+
+    // if (input === questions.answer) {
+    //     score ++;
+    //     secondsLeft = secondsLeft + 15;
+    // }
+    // else {
+    //     secondsLeft = secondsLeft - 15;
+    // }
+};
 
 
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
+    {
+        title: "something something.",
+        options: ["thing", "some", "something", "nothing"],
+        answer: "something"
+
+    }
+
+
+
     ///etc.
   ];
-
-
-
-
-
-
-
-
-
-//game starts with a score of 0 (display at end of game)
-var score = 0;
-
-    // Loop over every question object
-    for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-        var answer = confirm(questions[i].q);
-  
-        // Compare answers
-        if ((answer === true && questions[i].a === "t") ||
-          (answer === false && questions[i].a === "f")) {
-          // Increase score
-          score++;
-          alert("Correct!");
-        }
-        else {
-          alert("Wrong!");
-        }
-      }
-  
-      // Show total at end
-      alert("You got " + score + "/" + questions.length);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//this event listener, upon click, will initiate the game
-// //there ?is a function also connected to the button to start the timer (seperate function)
-// startGame.addEventListener("click", function(){
-
-// });
-
-//timer function (will be initiated every time the use presses Next 15 seconds each)
-// var c = 0;
-// var t;
-// var timer_is_on = 0;
-
-// function timedCount() {
-//   document.getElementById("txt").value = c;
-//   c = c + 1;
-//   t = setTimeout(timedCount, 1000);
-// }
-
-// function startCount() {
-//   if (!timer_is_on) {
-//     timer_is_on = 1;
-//     timedCount();
-//   }
-// }
-
-// function stopCount() {
-//   clearTimeout(t);
-//   timer_is_on = 0;
-// }
-
-
-
