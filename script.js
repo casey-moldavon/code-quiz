a4f34c8e80c33b3ee35a904e6cf3f066e6e1b24c
Casey Yang <cmoldavon@gmail.com>
2019-12-06 13:36:30 -0800

    Adding high-score button and updating style.css

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
 
 //these are the quiz options
-var questionDisplay = document.querySelector("#question-display");
+var questionDisplay = document.getElementById("question-display");
 var option1 = document.getElementById("op1");
+option1.style.visibility = "hidden"
 var option2 = document.getElementById("op2");
+option2.style.visibility = "hidden"
 var option3 = document.getElementById("op3");
+option3.style.visibility = "hidden"
 var option4 = document.getElementById("op4");
+option4.style.visibility = "hidden"
 var startButton = document.getElementById("start-button");
+var highScoreButton = document.getElementById("high-scores");
 var timeEl = document.querySelector(".timer");
+
+
 var score = 0
+
+
+//------------------------------ Question Array ------------------------------------
+var questionArray = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "something something.",
+        options: ["thing", "some", "something", "nothing"],
+        answer: "something"
+
+    }
+    ///etc.
+  ];
+
+
+//*****important notes*****
+//give additional points for answering questions faster (possibly create hidden timer that starts per iteration of questions to award bonus points)
+  //if statement (if answer before hiddenTimer == 0, +5%)
+//add button to access high scores from first page
+//store final score in local storage
+//hide elements that are not present (before start button is pressed)
+//aftrer quiz is complete, give prompt or possibly change page (for user to enter initials)
+//initials and score are stored in a list that pulls from local storage.
+//list must display top score to bottom score (litterally).
+
+//bonus: sound bloops
+
 
 
 //-------------------------- function to start quiz ---------------------------------
 startButton.addEventListener("click", function() {
     console.log("happy thoughts");
-    quizQuestions();
+    displayQuestions();
     setTime();
     startButton.style.visibility = "hidden";
+    highScoreButton.style.visibility = "hidden";
 })
 
 
@@ -39,14 +83,71 @@ function setTime() {
 
 // ------------------------------ Quiz Questions ------------------------------------
 
-function quizQuestions() {
-    var index = 0
-    questionDisplay = questions[0].title;
-    option1 = questions[0].options[0];
-    option2 = questions[0].options[1];
-    option3 = questions[0].options[2];
-    option4 = questions[0].options[3];
-    index ++
+var index = 0;
+var input;
+function displayQuestions() {
+
+    //not needed:
+    // var ques = questionArray[index]
+    // questionDisplay.innertext = ques.title;
+    
+
+    questionDisplay.textContent = questionArray[index].title;
+    option1.textContent = questionArray[index].options[0];
+    option2.textContent = questionArray[index].options[1];
+    option3.textContent = questionArray[index].options[2];
+    option4.textContent = questionArray[index].options[3];
+    index ++;
+
+    option1.style.visibility = "visible";
+    option2.style.visibility = "visible";
+    option3.style.visibility = "visible";
+    option4.style.visibility = "visible";
+
+//not done yet...
+    // if (questionArray[index].options[0] === questionArray[index].answer) {
+    //     score ++;
+    // }
+    // else {
+    //     secondsLeft = secondsLeft -15;
+    // }
+
+    
+
+
+    // function clickOption1(input){
+    //     input = option1
+    // };
+    // function clickOption2(input){
+    //     input = option2
+    // };
+    // function clickOption3(input){
+    //     input = option3
+    // };
+    // function clickOption4(input){
+    //     input = option4
+    // };
+
+
+//------------------------ next page -------------------------------------------
+
+    // nextPage();
+
+    // if (input === questionArray.answer) {
+    //     score++;
+    // };
+    // else {
+    //     secondsLeft = secondsLeft -15;
+    // };
+};
+
+
+function nextPage() {
+    console.log("Banana!");
+    displayQuestions();
+}
+
+
 
     // var input = //this variable represents the user's answer
 
@@ -57,28 +158,4 @@ function quizQuestions() {
     // else {
     //     secondsLeft = secondsLeft - 15;
     // }
-};
 
-
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        options: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "something something.",
-        options: ["thing", "some", "something", "nothing"],
-        answer: "something"
-
-    }
-
-
-
-    ///etc.
-  ];
